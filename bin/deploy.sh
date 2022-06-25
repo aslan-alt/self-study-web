59e70feecd6b2c9c32e535fb40cf5b76469e58fe
JingSong Xiong <54422571+aslan-alt@users.noreply.github.com>
2022-06-25 16:27:56 +0800

    Revert "Patch: test"

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,9 +5,8 @@ yarn install --production=false
 yarn build
 yarn typeorm:build
 yarn migration:run
-docker login -u 874470891 -p 874470891xS!
-docker pull 874470891/self-study-web
-docker kill 874470891/self-study-web
-docker rm 874470891/self-study-web
-docker run --name app --network=host -p 3000:3000 -d 874470891/self-study-web
+docker build -t aslanxiong/node-web-app .
+docker kill app
+docker rm app
+docker run --name app --network=host -p 3000:3000 -d aslanxiong/node-web-app
 echo 'OK'
