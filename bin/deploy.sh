6fbb193b7df712934841d7fe3534d01f3acb2285
Jingsong Xiong <jingsong.xiong@compass.com>
2022-06-25 17:16:12 +0800

    Patch: test

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,7 +7,7 @@ yarn typeorm:build
 yarn migration:run
 docker login -u 874470891 -p 874470891xS!
 docker pull 874470891/self-study-web
-docker kill 874470891/self-study-web
-docker rm 874470891/self-study-web
+docker kill app
+docker rm app
 docker run --name app --network=host -p 3000:3000 -d 874470891/self-study-web
 echo 'OK'
