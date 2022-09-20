1093ce3be2865a049f4f31aa138112f9b896f637
Jingsong Xiong <jingsong.xiong@compass.com>
2022-09-20 11:59:25 +0800

    Patch: add upload.ts

--- components/Lessons.tsx
+++ components/Lessons.tsx
@@ -28,7 +28,6 @@ export const Lessons = () => {
           </Item>
         );
       })}
-      s
     </Container>
   );
 };
---
+++ pages/api/v1/merge.ts
@@ -0,0 +1,29 @@
+import fs from 'fs';
+import path from 'path';
+import fse from 'fs-extra';
+import {NextApiHandler} from 'next';
+import {videoUploadDir} from '@/constants/index';
+
+const Merge: NextApiHandler = async (req, res) => {
+  const needMergeFilePath = path.join(videoUploadDir, req.body.chunkDir);
+  const chunks = await fse.readdir(needMergeFilePath);
+  try {
+    chunks
+      .sort((a, b) => {
+        const [aIndex] = a.split('.');
+        const [bIndex] = b.split('.');
+        return Number(aIndex) - Number(bIndex);
+      })
+      .map((chunkPath) => {
+        fs.appendFileSync(
+          needMergeFilePath + '.mp4',
+          fs.readFileSync(path.join(needMergeFilePath, chunkPath))
+        );
+      });
+    res.status(200).json({status: 'success'});
+  } catch (error) {
+    res.status(500).json({status: 'failed', error});
+  }
+};
+
+export default Merge;
