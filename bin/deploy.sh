b7d255ce4969d60a7c3636ff6cd8086a0c4656b7
Jingsong Xiong <jingsong.xiong@compass.com>
2022-09-25 13:14:10 +0800

    Fix:[FL-6] 修复 nginx服务器无法返回静态资源 [QA:None] Follow:https://pilp20220912063421218.pingcode.com/pjm/items/632fe2c419a7831ded591e2b?%20=#FL-6

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -8,6 +8,5 @@ docker login -u 874470891 -p 874470891xS! &&
 docker pull 874470891/self-study-web &&
 docker kill app &&
 docker rm app &&
-docker run --privileged=true -v "$PWD/videos":/usr/src/app/videos --name app --network=host -p 3000:3000 -d 874470891/self-study-web &&
-echo 'OK' &&
-
+docker run --privileged=true -v "$PWD/videos":/usr/src/app/videos -v "$PWD/.next":/usr/src/app/.next --name app --network=host -p 3000:3000 -d 874470891/self-study-web &&
+echo 'OK'
