6fce4a39124e1ac97a29557cc0a9e0319cf97144
Jingsong Xiong <jingsong.xiong@compass.com>
2022-12-10 13:13:22 +0800

    Patch: test

--- pages/api/Video/getVideos.ts
+++ pages/api/Video/getVideos.ts
@@ -1,16 +1,15 @@
 import {NextApiHandler} from 'next';
-// import {Video} from '@/DB/entity';
-// import {getConnection} from '@/DB/getConnection';
+import {Video} from '@/DB/entity';
+import {getConnection} from '@/DB/getConnection';
 
 const getVideos: NextApiHandler = async (req, res) => {
-  // const connection = await getConnection();
-  res.status(200).json([]);
-  // try {
-  //   const videos = await connection.getRepository(Video).find({where: {course: {id: req.body.id}}});
-  //   res.status(200).json(videos);
-  // } catch (error) {
-  //   res.status(500).json({error, message: 'Failed to get data'});
-  // }
+  const connection = await getConnection();
+  try {
+    const videos = await connection.getRepository(Video).find({where: {course: {id: req.body.id}}});
+    res.status(200).json(videos);
+  } catch (error) {
+    res.status(500).json({error, message: 'Failed to get data'});
+  }
 };
 
 export default getVideos;
