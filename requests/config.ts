e3ea00b424d7d3fb9967c6b72472c1a21e6bb7f5
Jingsong Xiong <jingsong.xiong@compass.com>
2022-12-11 14:04:58 +0800

    Patch: remove axios.defaults.baseURL

--- requests/config.ts
+++ requests/config.ts
@@ -1,6 +1,3 @@
-import axios from 'axios';
-axios.defaults.baseURL = 'http://localhost:3000';
-
 export const api = {
   createCourse: '/api/Course/createCourse',
   getAllCourses: '/api/Course/getAllCourses',
